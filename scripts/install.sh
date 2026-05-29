2778a7e46472bdbe797f9ba9b225e65fa0a4154e
Ethan Bork <ebork11@gmail.com>
2026-05-29 14:40:49 -0700

    move install install script to scripts folder

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# System packages
+packages=(
+  ttf-jetbrains-mono-nerd
+  zsh
+  neovim
+  firefox
+  rofi
+  picom
+  feh
+  i3
+  ghostty
+  openssh
+  pkgfile
+  git
+)
+
+sudo pacman -S --noconfirm "${packages[@]}"
+
+# Refresh font cache
+fc-cache -fv
